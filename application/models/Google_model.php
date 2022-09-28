57b2ab86bb0eb39b8162f464ddf06e505de1a382
johnernest11 <johnernest.catungal1997@gmail.com>
2022-09-29 00:17:15 +0800

    0

--- application/models/Google_model.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	class Google_model extends CI_Model{
-		public function getUser($email){
-			return $this->db->where('Email',$email)->get('admin')->row();
-		}
-		public function createUser($data){
-			$this->db->insert('admin',$data);
-		}
-	}
--- application/models/User_model.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	class User_model extends CI_Model{
-		public function getUser($email){
-			return $this->db->where('email',$email)->get('admin')->row();
-		}
-		public function createUser($data){
-			$this->db->insert('admin',$data);
-		}
-	}
